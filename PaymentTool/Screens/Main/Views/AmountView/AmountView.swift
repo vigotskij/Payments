b69082ac9d2668c1480928c0a7c2f5d4eb8dc783
Boris Sortino <boris.sortino@gmail.com>
2020-07-15 22:05:17 -0300

    Adapted AmountView to Hideable and NibLoadable protocols.

--- PaymentTool/Screens/Main/Views/AmountView/AmountView.swift
+++ PaymentTool/Screens/Main/Views/AmountView/AmountView.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-final class AmountView: UIView {
+final class AmountView: UIView, NibLoadable {
     private var viewModel: MainModels.AmountViewModel? {
         didSet {
             title?.text = viewModel?.title
@@ -16,9 +16,36 @@ final class AmountView: UIView {
     }
 
     // MARK: Outlets
-    @IBOutlet private weak var title: UILabel?
+    @IBOutlet private weak var title: UILabel? {
+        didSet {
+            title?.text = viewModel?.title
+        }
+    }
 
     func update(viewModel: MainModels.AmountViewModel?) {
         self.viewModel = viewModel
+    }
+
+    // MARK: - Initializers
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        commonInit()
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        commonInit()
+    }
+}
+
+private extension AmountView {
+    func commonInit() {
+        loadNibContent()
+    }
+}
+
+extension AmountView: Hideable {
+    func shouldHide() -> Bool {
+        return viewModel == nil
     }
 }
