bfb3960d3c528f86434033a6bd64e657961b9e41
Boris Sortino <boris.sortino@gmail.com>
2020-07-15 22:09:04 -0300

    Adapted SuccessView to Hideable and NibLoadable protocols.

--- PaymentTool/Screens/Main/Views/SuccessView/SuccessView.swift
+++ PaymentTool/Screens/Main/Views/SuccessView/SuccessView.swift
@@ -8,14 +8,33 @@
 
 import UIKit
 
-class SuccessView: UIView {
+final class SuccessView: UIView, NibLoadable {
+    private var viewModel: MainModels.SuccessViewModel?
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    // MARK: - Initializers
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        commonInit()
     }
-    */
 
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        commonInit()
+    }
+
+    func update(viewModel: MainModels.SuccessViewModel?) {
+        self.viewModel = viewModel
+    }
+}
+
+private extension SuccessView {
+    func commonInit() {
+        loadNibContent()
+    }
+}
+
+extension SuccessView: Hideable {
+    func shouldHide() -> Bool {
+        return viewModel == nil
+    }
 }
