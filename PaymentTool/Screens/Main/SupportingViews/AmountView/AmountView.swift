9c45c144d1e2e5bd94bc30ff49521d974755ec93
Boris Sortino <boris.sortino@gmail.com>
2020-07-17 19:44:00 -0300

    added keyboard functionality to AmountView

--- PaymentTool/Screens/Main/SupportingViews/AmountView/AmountView.swift
+++ PaymentTool/Screens/Main/SupportingViews/AmountView/AmountView.swift
@@ -8,15 +8,43 @@
 
 import UIKit
 
-final class AmountView: UIView, NibLoadable {
+final class AmountView: UIView, NibLoadable, UITextInputTraits {
     private var viewModel: MainModels.AmountViewModel? {
         didSet {
             title?.text = viewModel?.amount
         }
     }
+    private var amount: String = "" {
+        didSet {
+            title?.text = formattedAmount
+        }
+    }
+    private var amountDouble: Double {
+        (Double(amount) ?? 0.0) * 0.01
+    }
+    private var formattedAmount: String {
+        String(format: "$ %.2f", amountDouble)
+    }
+
+    // MARK: Public properties
+    var keyboardType: UIKeyboardType = .numberPad
+    var didFinishedEnterAmount: ((String) -> Void)?
 
     // MARK: Outlets
     @IBOutlet private weak var title: UILabel?
+
+    // MARK: Actions
+    @IBAction func continueButtonTapped(_ sender: UIButton?) {
+        if let didFinishedEnterAmount = didFinishedEnterAmount {
+            if amountDouble.isValidAmount() {
+                resignFirstResponder()
+                // MARK: adding delay so it looks like animated
+                DispatchQueue.main.asyncAfter(deadline: .now() + 0.3) {
+                    didFinishedEnterAmount(self.formattedAmount)
+                }
+            }
+        }
+    }
 
     func update(viewModel: MainModels.AmountViewModel?) {
         self.viewModel = viewModel
@@ -43,5 +71,28 @@ private extension AmountView {
 extension AmountView: Hideable {
     func shouldHide() -> Bool {
         return viewModel == nil
+    }
+}
+
+extension AmountView {
+    override var canBecomeFirstResponder: Bool {
+        return true
+    }
+}
+
+// MARK: - UIKeyInput
+extension AmountView: UIKeyInput {
+    var hasText: Bool {
+        return amount.count > 0
+    }
+
+    func insertText(_ text: String) {
+        amount.append(contentsOf: text)
+    }
+
+    func deleteBackward() {
+        if hasText {
+            amount.removeLast()
+        }
     }
 }
